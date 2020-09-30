6624bd05edaa846260e49af784d943b965e9c17d
Chance Smith <chancesmith97@hotmail.com>
2020-09-30 23:06:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,31 +4,31 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function(arr, cb) {
-      if (Array.isArray(arr)){
-        for (let i = 0; i < arr.length; i++){
-          cb(arr[i], i, arr);
+    each: function(coll, cb) {
+      if (Array.isArray(coll)){
+        for (let i = 0; i < coll.length; i++){
+          cb(arr[i], i, coll);
         }
-        return arr;
+        return coll;
       }  else {
-        for (let e in arr){
-          cb(arr[e], e, arr);
+        for (let e in coll){
+          cb(coll[e], e, arr);
         }
         return arr;
       }
     },
 
-    map: function(arr, cb) {
-      if (Array.isArray(arr)){
+    map: function(coll, cb) {
+      if (Array.isArray(coll)){
         let newArr = [];
-        for (let i = 0; i < arr.length; i++){
-          newArr.push(cb(arr[i]));
+        for (let i = 0; i < coll.length; i++){
+          newArr.push(cb(coll[i]));
         }
         return newArr;
       } else {
         let newArr = [];
-        for (let e in arr){
-          newArr.push(cb(arr[e]));
+        for (let e in coll){
+          newArr.push(cb(coll[e]));
         }
         return newArr;
       }
