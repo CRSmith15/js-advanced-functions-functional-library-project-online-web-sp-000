2dc71127ebae616a7f3ff198e0e67740ebb0422a
Chance Smith <chancesmith97@hotmail.com>
2020-09-30 23:02:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(arr, cb) {
+      if (Array.isArray(arr)){
+        for (let i = 0; i < arr.length; i++){
+          cb(arr[i], i, arr);
+        }
+        return arr;
+      }  else {
+        for (let e in arr){
+          cb(arr[e], e, arr);
+        }
+        return arr;
+      }
     },
 
     map: function() {
